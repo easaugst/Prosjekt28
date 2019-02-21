d89fad94b9581fa3e0448f2b9a02d6edbf7e63ac
easaugst <44259036+easaugst@users.noreply.github.com>
2019-02-21 11:05:03 +0100

    Update Utkast1/src/services.js

--- Utkast1/src/services.js
+++ Utkast1/src/services.js
@@ -13,7 +13,7 @@ class UtstyrService {
 
 class SykkelService {
   getSykkel(sykkelid, success) {
-    connection.query('SELECT * FROM Sykkel', [sykkelid], (error, results) => {
+    connection.query('SELECT * FROM Sykkel S, Sykkeltype ST WHERE S.sykkeltypeid = ST.sykkeltypeid', [sykkelid], (error, results) => {
 
       if(error) return console.error(error);
 
