c40307ea98aa3e3d3a5bfe30aba287c31bd3e568
easaugst <44259036+easaugst@users.noreply.github.com>
2019-02-21 12:35:19 +0100

    Update Utkast1/src/services.js

--- Utkast1/src/services.js
+++ Utkast1/src/services.js
@@ -9,6 +9,7 @@ class UtstyrService {
         success(results);
       });
   }
+
 }
 
 class SykkelService {
@@ -32,6 +33,14 @@ class KundeService {
       success(results);
     });
   }
+
+  updateKunde(kundenr, fnavn, enavn, epost, tlf, success) {
+  connection.query('update Kunde set fnavn=?, enavn =?, epost=?, tlf =? where kundenr=?', [fnavn, enavn, epost, tlf, kundenr], (error, results) => {
+    if (error) return console.error(error);
+
+    success();
+  });
+}
 }
 
 class BestillingsService {
