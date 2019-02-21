31b32bf5cae878306d391eb404da1d7d357f4cf5
easaugst <44259036+easaugst@users.noreply.github.com>
2019-02-21 11:31:25 +0100

    Update Utkast1/src/services.js

--- Utkast1/src/services.js
+++ Utkast1/src/services.js
@@ -25,7 +25,18 @@ class SykkelService {
 
 class KundeService {
   getKunde(kundeid, success) {
-    connection.query('SELECT * FROM Kunde', [kundeid], (error, results) => {
+    connection.query('SELECT * FROM Kunde', [kundenr], (error, results) => {
+
+      if(error) return console.error(error);
+
+      success(results);
+    });
+  }
+}
+
+class BestillingsService {
+  getBestilling(bestillingsid, success) {
+    connection.query('SELECT * FROM Bestilling', [bestillingsid], (error, results) => {
 
       if(error) return console.error(error);
 
@@ -71,3 +82,4 @@ class KundeService {
 export let utstyrService = new UtstyrService();
 export let sykkelService = new SykkelService();
 export let kundeService = new KundeService();
+export let bestillingsService new BestillingsService();
