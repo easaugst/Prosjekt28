a9cd2caefd327302246797c464f9fc3a85677191
LoudBanshee <simon@morlandfamily.com>
2019-02-28 12:41:29 +0100

    Kule ting

--- Utkast1/src/index.js
+++ Utkast1/src/index.js
@@ -210,6 +210,7 @@ class SykkelOversikt extends Component {
 
 class KundeOversikt extends Component {
   kArray = [];
+  tid = '';
 
   render() {
     return (
@@ -221,6 +222,7 @@ class KundeOversikt extends Component {
             <th>Etternavn</th>
             <th>E-post</th>
             <th>Telefonnummer</th>
+            <th>Tid registrert</th>
           </Table.Rad>
           {this.kArray.map(kunde => (
             <Table.Rad key={kunde.kundenr}>
@@ -229,6 +231,7 @@ class KundeOversikt extends Component {
               <td>{kunde.enavn}</td>
               <td>{kunde.epost}</td>
               <td>{kunde.tlf}</td>
+              <td>{JSON.stringify(kunde.rtid).replace(/T|Z|"/g, " ").slice(0, -6)}</td>
             </Table.Rad>
           ))}
         </Table>
