60ef0eea5501553ac49fa7695e7e92d1813ae816
Peder Nielsen <pednie97@gmail.com>
2019-03-01 13:21:09 +0100

    antall utstyr

--- Utkast1/src/index.js
+++ Utkast1/src/index.js
@@ -50,6 +50,18 @@ class Utleie extends Component {
                 <option>Bagasjebrett</option>
                 <option>Sykkelveske</option>
               </select>
+              <select className="form-control" placeholder="Antall">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+              </select>
               <label>Type leie</label>
               <select className="form-control">
                 <option>Timesutleie</option>
