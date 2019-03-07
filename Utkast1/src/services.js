12fef4fa6a501422bb153a387a045678fb0a9dea
easaugst <44259036+easaugst@users.noreply.github.com>
2019-03-07 11:43:29 +0100

    Update services.js

--- Utkast1/src/services.js
+++ Utkast1/src/services.js
@@ -145,6 +145,30 @@ class UtleieService {
     });
   }
 
+    addBestilling(bestillingsid, kundenr, utleietype, kontant, btid, ftid, ttid, gruppe, success) {
+      connection.query(
+        'insert into Bestilling (kundenr, utleietype, kontant, ftid, ttid, gruppe) values (?, ?, ?, ?, ?,?)',
+        [kundenr, utleietype, kontant, ftid,  ttid, gruppe],
+        (error, results) => {
+          if (error) return console.error(error);
+
+          success();
+        }
+      );
+    }
+
+    addUBestilling(ubid, regnr, utstyrsid, detaljer, bestillingsid, success) {
+      connection.query(
+        'insert into Ubestilling (regnr, utstyrsid, detaljer, bestillingsid) values (?, ?, ?, ?)',
+        [regnr, utstyrsid, detaljer, bestillingsid],
+        (error, results) => {
+          if (error) return console.error(error);
+
+          success();
+        }
+      );
+    }
+
 }
 
 //LA ATÅ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
