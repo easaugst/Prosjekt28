7ddac3ae661bb7d971f1dff421ab8ac87af9c589
Torkel <torkel.n@hotmail.com>
2019-03-11 14:44:26 +0100

    update ansatt

--- Utkast1/src/Components/Endring/Ansatt.js
+++ Utkast1/src/Components/Endring/Ansatt.js
@@ -79,6 +79,8 @@ export class AnsattEndringMeny extends Component {
           <Form.Label>Etternavn:</Form.Label>
           <Form.Input type="text" value={this.enavn} onChange={event => (this.enavn = event.target.value)} />
 
+          <Form.Label>Admin:</Form.Label>
+
           <select className="form-control" form="formen" onChange={event => (this.admin = event.target.value)}>
             <option>Er hen admin?</option>
             <option value="0">Nei</option>
