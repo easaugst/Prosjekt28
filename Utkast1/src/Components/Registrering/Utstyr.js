eb2a8b18b41847c4e9b7fb49c28b376bcd52989f
easaugst <44259036+easaugst@users.noreply.github.com>
2019-03-21 14:25:56 +0100

    Update Utstyr.js

--- Utkast1/src/Components/Registrering/Utstyr.js
+++ Utkast1/src/Components/Registrering/Utstyr.js
@@ -25,9 +25,9 @@ export class UtstyrReg extends Component {
         <Card title="Utstyrsinformasjon">
           <Form.Label>Utstyrstype:</Form.Label>
           <select className="form-control" form="formen" onChange={event => (this.utstyrstypeid = event.target.value)}>
-            <option>Velg type her</option>
-            <option value="1">Hjelm</option>
-            <option value="2">Lappesett</option>
+            <option value="4">Velg type her</option>
+            <option value="4">Hjelm</option>
+            <option value="5">Lappesett</option>
           </select>
           <Form.Label>Utstyrstatus:</Form.Label>
           <Form.Input type="text" value={this.ustatus} onChange={event => (this.ustatus = event.target.value)} />
@@ -36,7 +36,7 @@ export class UtstyrReg extends Component {
 
         <div className="knapper">
           <span className="tilbakeMeny2">
-            <Button.Success onClick={this.save}>
+            <Button.Success onClick={this.add}>
               Registrer utstyr
             </Button.Success>
           </span>
