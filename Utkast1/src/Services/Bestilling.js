54707098953319693d67439930309aa5deb63478
easaugst <44259036+easaugst@users.noreply.github.com>
2019-03-21 13:39:09 +0100

    Update Bestilling.js

--- Utkast1/src/Services/Bestilling.js
+++ Utkast1/src/Services/Bestilling.js
@@ -10,7 +10,7 @@ class BestillingsService {
   }
 
   getDelbestilling(bestillingsid, ubid, success) {
-    connection.query('SELECT Distinct B.bestillingsid, U.regnr, U.ubid, U.utstyrsid, UT.utnavn FROM Ubestilling U, Bestilling B, Sykkel S, Utstyr UY, Utleietype UT where U.bestillingsid = B.bestillingsid and U.bestillingsid = ? AND S.regnr = U.regnr AND S.sykkeltypeid = UT.utid OR (U.bestillingsid = B.bestillingsid and U.bestillingsid = ? AND UY.utstyrsid = U.utstyrsid AND UY.utstyrstypeid = UT.utid) ORDER BY U.ubid', [bestillingsid, ubid], (error, results) => {
+    connection.query('SELECT Distinct B.bestillingsid, U.regnr, U.ubid, U.utstyrsid, UT.utnavn FROM Ubestilling U, Bestilling B, Sykkel S, Utstyr UY, Utleietype UT where U.bestillingsid = B.bestillingsid and U.bestillingsid = ? AND S.regnr = U.regnr AND S.sykkeltypeid = UT.utid OR (U.bestillingsid = B.bestillingsid and U.bestillingsid = ? AND UY.utstyrsid = U.utstyrsid AND UY.utstyrstypeid = UT.utid) ORDER BY U.ubid', [bestillingsid, bestillingsid, ubid], (error, results) => {
       if (error) return console.error(error);
 
       success(results);
