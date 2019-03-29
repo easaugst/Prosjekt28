fd8ba289968a0cc890689d79a49c7de8673c55d9
easaugst <44259036+easaugst@users.noreply.github.com>
2019-03-29 12:22:08 +0100

    asdad

--- Utkast1/src/Components/Registrering/Sykkel.js
+++ Utkast1/src/Components/Registrering/Sykkel.js
@@ -45,6 +45,7 @@ export class SykkelReg extends Component {
 
           <Form.Label>Status:</Form.Label>
           <select className="form-control" form="formen" value={this.status} onChange={event => (this.status = event.target.value)}>
+          <option value="Lager">Velg status</option>
           <option value="Lager">Lager</option>
           <option value="Utleid">Utleid</option>
           <option value="Service">Service</option>
--- Utkast1/src/Components/Registrering/Utstyr.js
+++ Utkast1/src/Components/Registrering/Utstyr.js
@@ -40,6 +40,7 @@ export class UtstyrReg extends Component {
 
           <Form.Label>Utstyrstatus:</Form.Label>
           <select className="form-control" form="formen" onChange={event => (this.ustatus = event.target.value)}>
+            <option value="Lager">Velg status</option>
             <option value="Lager">Lager</option>
             <option value="Utleid">Utleid</option>
             <option value="Service">Service</option>
